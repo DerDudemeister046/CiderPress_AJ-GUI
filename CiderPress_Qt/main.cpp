f1852f2ca7b8649d88d46b565aa508b488493dd3
Andreas Amzoll <amzoll.andreas@googlemail.com>
2019-12-19 11:34:16 +0100

    added new functions
    Added get and set functions to XML_Manager class.
    Added .gitignore as well.

--- CiderPress_Qt/main.cpp
+++ CiderPress_Qt/main.cpp
@@ -1,11 +1,15 @@
 #include "ciderpress_gui.h"
 
 #include <QApplication>
+#include "xml_manager.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    CiderPress_Gui w;
-    w.show();
+    //CiderPress_Gui w;
+    //w.show();
+
+    XML_Manager *xml = new XML_Manager;
+
     return a.exec();
 }
--- CiderPress_Qt/xml_manager.h
+++ CiderPress_Qt/xml_manager.h
@@ -10,6 +10,9 @@
 #define XML_MANAGER_H
 
 #include <QtCore>
+#include <QFile>            // to manage files with this class
+#include <QtXml>            // xml implementation
+#include <QDomDocument>     // to handle xml documents
 
 class XML_Manager
 {
@@ -18,13 +21,20 @@ public:
     XML_Manager();
     ~XML_Manager();
     // GET
-    QString getFilename();
+    QDomDocument getDocument();
+    QDomElement getRoot();
+    QString getFilename();              // returns filename of file
     // SET
-    void setFilename(QString filename);
+    void setDocument(QDomDocument document);
+    void setRoot(QDomElement root);
+    void setFilename(QString filename); // set name of file to be read/writen from/to
     // Class functions
+    void test();    // Testing function to be deleted in final version
     // Checker functions
 private:
-    QString filename; // name of the file to be read or written to.
+    QDomDocument document;  // xml document handle
+    QDomElement root;       // root element of document
+    QString filename;       // name of the file to be read or written to.
 };
 
 #endif // XML_MANAGER_H
