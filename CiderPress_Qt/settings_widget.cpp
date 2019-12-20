73f313fa41227810f6f097e502942fafb82408d6
Andi <andi@stux-haven.de>
2019-12-20 14:01:50 +0100

    Created Settings_Widget UI
    Ui was created. basic classes were created. no functionality yet.

--- CiderPress_Qt/settings_widget.cpp
+++ CiderPress_Qt/settings_widget.cpp
@@ -12,3 +12,30 @@ Settings_Widget::~Settings_Widget()
 {
     delete ui;
 }
+
+// Slots and Signals
+
+void Settings_Widget::on_ok_btn_clicked()
+{
+
+}
+
+void Settings_Widget::on_cancel_btn_clicked()
+{
+
+}
+
+void Settings_Widget::on_browse_temp_btn_clicked()
+{
+
+}
+
+void Settings_Widget::on_browse_finish_btn_clicked()
+{
+
+}
+
+void Settings_Widget::on_browse_upload_btn_clicked()
+{
+
+}
